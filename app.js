5df787a7ffc0613d9b27a9d4f90e6a4dd182bc4e
Jenia Brook <jeniabrook@gmail.com>
2016-11-07 20:54:48 +0200

    Added app.js for fluid scroling and counter for message, fixed the container for form, now it's responsive.

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#characterLeft').text('300 characters left');
+    $('#message').keydown(function () {
+        var max = 300;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('You have reached the limit');
+            $('#characterLeft').addClass('red');
+            $('#btnSubmit').addClass('disabled');
+        }
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left');
+            $('#btnSubmit').removeClass('disabled');
+            $('#characterLeft').removeClass('red');
+        }
+    });
+
+    $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+});
